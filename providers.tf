c41d6d8d745fed771e43f5ce48da0a7b7f0b958a
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-09-30 12:09:31 +0530

    aksauto

--- providers.tf
+++ providers.tf
@@ -13,6 +13,6 @@ provider "azurerm" {
 #   subscription_id = "02a44fee-b200-4cf9-b042-9bd4aa3bebe6"
 # tenant_id = "63b9a1c1-375c-42cf-9c63-dc3798c7ae5e"
 # use_cli = true
-    use_msi         = true
-#   client_id       = "7b4aa901-d916-453a-b989-43bc3c0cf6cd"
+    # use_msi         = true
+client_id       = "d19bf781-2ee2-4b86-9056-f16dea012983"
 }
