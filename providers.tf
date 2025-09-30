b0dcec01bc1390291f56f910ca0cc006ee086d31
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-09-30 12:05:27 +0530

    aksauto

--- providers.tf
+++ providers.tf
@@ -10,9 +10,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "02a44fee-b200-4cf9-b042-9bd4aa3bebe6"
-tenant_id = "63b9a1c1-375c-42cf-9c63-dc3798c7ae5e"
-use_cli = true
-    # use_msi         = true
+#   subscription_id = "02a44fee-b200-4cf9-b042-9bd4aa3bebe6"
+# tenant_id = "63b9a1c1-375c-42cf-9c63-dc3798c7ae5e"
+# use_cli = true
+    use_msi         = true
 #   client_id       = "7b4aa901-d916-453a-b989-43bc3c0cf6cd"
 }
