ca9247ffbd13c89627eeb0644f559b34a1d3a4d9
Balamurali Ramakrishnan <balamurali.ramakrishnan@infoservices.com>
2025-09-30 11:00:55 +0530

    added new changes

---
+++ main.tf
@@ -0,0 +1,52 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.prefix}-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-dns"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = var.node_count
+    vm_size    = var.node_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  linux_profile {
+    admin_username = "azureuser"
+    ssh_key {
+      key_data = file(var.ssh_pub_key)
+    }
+  }
+
+    oms_agent {
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+    }
+
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+   # RBAC is now an attribute, not a block
+#   enable_rbac = true
+
+#   role_based_access_control {
+#     enabled = true
+#   }
+}
---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "02a44fee-b200-4cf9-b042-9bd4aa3bebe6"
+tenant_id = "63b9a1c1-375c-42cf-9c63-dc3798c7ae5e"
+    # use_msi         = true
+#   client_id       = "7b4aa901-d916-453a-b989-43bc3c0cf6cd"
+}
